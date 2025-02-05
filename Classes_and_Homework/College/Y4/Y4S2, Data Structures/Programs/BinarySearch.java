49046227e85481a7b57b2a5a41f8eea88c8811be
ai-bearing <120073191+ai-bearing@users.noreply.github.com>
2025-02-04 21:12:39 -0800

    various updates

--- Classes_and_Homework/College/Y4/Y4S2, Data Structures/Programs/BinarySearch.java
+++ Classes_and_Homework/College/Y4/Y4S2, Data Structures/Programs/BinarySearch.java
@@ -4,8 +4,9 @@ public class BinarySearch{
     int[] numbers = {2, 4, 7, 10, 11, 32, 45, 87};
     System.out.println("The array is {2, 4, 7, 10, 11, 32, 45, 87}");
     Scanner input = new Scanner(System.in);
-    int boundaryIndex = numbers.length - 1;
-    int searchIndex = boundaryIndex/2;
+    int upperIndex = numbers.length - 1;
+    int lowerIndex = 0;
+    int searchIndex = (upperIndex + lowerIndex)/2;
     /* Start at searchIndex, check value of input vs. numbers[searchIndex].
      * If input = numbers[SearchIndex], break.
      * If input > numbers[searchIndex], then change searchIndex to boundaryIndex - (boundaryIndex - searchIndex)/2
@@ -15,25 +16,41 @@ public class BinarySearch{
     int key = input.nextInt();
     System.out.println();
     boolean notFound = true;
-    boolean notAtEdgeIndex = true;
-    while(notFound && notAtEdgeIndex){
-      if(searchIndex == numbers.length-1 || searchIndex == 0){
-        notAtEdgeIndex = false;
-      }
+    while(notFound && (upperIndex > lowerIndex)){
       System.out.println("Searching for key value " + key + " at index " + searchIndex + "...");
       if(key == numbers[searchIndex]){
+        System.out.println("Finding key value at index " + searchIndex);
         notFound = false;
       } else if (key > numbers[searchIndex]){
-        searchIndex = boundaryIndex - (boundaryIndex - searchIndex)/2;
+        //System.out.println("Key too large at index " + searchIndex);
+        lowerIndex = searchIndex;
+        searchIndex = (upperIndex + lowerIndex)/2;
+//        System.out.println("Searching between indices " + lowerIndex + " and " + upperIndex);
       } else if (key < numbers[searchIndex]){
-        boundaryIndex = boundaryIndex / 2;
-        searchIndex = searchIndex / 2;
+        //System.out.println("Key too small at index " + searchIndex);
+        upperIndex = upperIndex / 2;
+        searchIndex = (upperIndex + lowerIndex)/2;
+        //System.out.println("Searching between indices " + lowerIndex + " and " + upperIndex);
       }
     }
-    if(notFound){
-      System.out.println("Key value not found");
-    } else{
-      System.out.println("Found key value " + key + " at index " + searchIndex);
-    }
+    // while(notFound && notAtEdgeIndex){
+    //   if(searchIndex == numbers.length-1 || searchIndex == 0){
+    //     notAtEdgeIndex = false;
+    //   }
+    //   System.out.println("Searching for key value " + key + " at index " + searchIndex + "...");
+    //   if(key == numbers[searchIndex]){
+    //     notFound = false;
+    //   } else if (key > numbers[searchIndex]){
+    //     searchIndex = boundaryIndex - (boundaryIndex - searchIndex)/2;
+    //   } else if (key < numbers[searchIndex]){
+    //     boundaryIndex = boundaryIndex / 2;
+    //     searchIndex = searchIndex / 2;
+    //   }
+    // }
+     if(notFound == true){
+       System.out.println("Key value not found");
+     } else{
+       System.out.println("Found key value " + key + " at index " + searchIndex);
+     }
   }
 }
