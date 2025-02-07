39ace8d68e9c3918f0a9ddf87ce712a38b259504
ai-bearing <120073191+ai-bearing@users.noreply.github.com>
2025-02-07 12:06:16 -0800

    some changes

---
+++ Classes_and_Homework/College/Y4/Y4S2, Data Structures/Programs/Homework Programs/LinearSearchSimulator.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class LinearSearchSimulator{
+  public static void main(String[] args){
+    Scanner input = new Scanner(System.in);
+    int[] nums = {1,2,3,4,5,6,7,8};
+    boolean isInvalid = true;
+    int validKey = -1;
+    while(isInvalid){
+      System.out.print("Please enter a number: ");
+      int key = input.nextInt();
+      if (key <= 8 && key >= 1){
+        validKey = key;
+        isInvalid = false;
+      }
+    }
+    int targetNum = -1;
+    System.out.println();
+    for(int i = 0; i < nums.length; i++){
+      System.out.println("Is the number " + nums[i] + "?");
+      if(nums[i] == validKey){
+        System.out.println("Yes");
+        targetNum = validKey;
+        break;
+      } else{
+        System.out.println("No");
+      }
+    }
+    if(targetNum > 0){
+      System.out.println("Your number is " + targetNum);
+    } else{
+      System.out.println("Your number was not found.");
+    }
+  }
+}
